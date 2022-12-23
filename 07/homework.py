fe0a65c786bfad3dd23f530658dc0c9d50ccec54
Volodymyr Rusin <volodymyr.rusin@gmail.com>
2022-12-23 16:01:43 +0200

    add homework 7

---
+++ 07/homework.py
@@ -0,0 +1,29 @@
+phone_book = {}
+while True:
+    command = input('Enter a command: ')
+    if command == 'stats':
+        print(f'Number of records is {len(phone_book.keys())}')
+    elif command == 'add':
+        name = input('Enter a name: ')
+        phone_number = input('Enter a phone number: ')
+        print(f'{name} is added to a phone book')
+        phone_book[name] = phone_number
+    elif command == 'delete':
+        name = input('Enter a name: ')
+        if name in phone_book.keys():
+            print(f'{name} is deleted from a phone book')
+            del phone_book[name]
+        else:
+            print('There is no such name in a phone book')
+    elif command == 'list':
+        print("List of names in a phone book:")
+        for key in phone_book.keys():
+            print(key)
+    elif command == 'show':
+        name = input('Enter a name: ')
+        if name in phone_book.keys():
+            print(f'{name} phone number is {phone_book[name]}')
+        else:
+            print('There is no such name in a phone book')
+    else:
+        print('There is no such command: ')
