c50a1b03dd32ab101ae24031106a5478252d0d72
Volodymyr Rusin <volodymyr.rusin@gmail.com>
2023-03-26 11:45:16 +0300

    changed homework 30

--- 30/homework.py
+++ 30/homework.py
@@ -9,12 +9,15 @@ print(f'Status code: {res.status_code}, Site name: {random_site}, Length of HTML
 
 # Task 2
 city_name = input('Please write name of the city: ')
-res1 = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={city_name}')
-data1 = res1.json()
-latitude = data1['results'][0]['latitude']
-longitude = data1['results'][0]['longitude']
-
-query_params2 = {'latitude': latitude, 'longitude': longitude, 'current_weather': True}
-res2 = requests.get('https://api.open-meteo.com/v1/forecast', params=query_params2)
-data2 = res2.json()
-print(data2['current_weather'])
+geo_res = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={city_name}')
+geo_data = geo_res.json()
+try:
+    latitude = geo_data['results'][0]['latitude']
+    longitude = geo_data['results'][0]['longitude']
+except KeyError:
+    print('Sorry but there is no such city')
+else:
+    query_params2 = {'latitude': latitude, 'longitude': longitude, 'current_weather': True}
+    weather_res = requests.get('https://api.open-meteo.com/v1/forecast', params=query_params2)
+    weather_data = weather_res.json()
+    print(weather_data['current_weather'])
