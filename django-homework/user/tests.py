2da82edc5011ee3c78a93ff8ec19812c540a972a
unknown <volodymyr.rusin@gmail.com>
2023-05-29 18:10:18 +0300

    homework 48

--- django-homework/user/tests.py
+++ django-homework/user/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import random
+import factory
 
-# Create your tests here.
+from django.test import TestCase
+from user.models import User
+
+
+class RandomUserFactory(factory.django.DjangoModelFactory):
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    age = random.randint(1, 100)
+
+    class Meta:
+        model = User
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = RandomUserFactory.create()
+
+    def test_user(self):
+        self.assertIsInstance(self.user, User)
+
+
+class UserViewSetTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = RandomUserFactory.create()
+
+    def test_user_create(self):
+        user_data = {
+            'first_name': 'Test user',
+            'age': 33
+        }
+        resp = self.client.post('/users/', data=user_data)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_get_user_detail(self):
+        resp = self.client.get(f'/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_user_remove(self):
+        resp = self.client.delete(f'/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 204)
